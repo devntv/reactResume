07675a7bfdd9c193741042c573ed7e81d8a57199
TheVinh <binkool281132442@gmail.com>
2020-05-07 23:09:09 +0700

    Updates

---
+++ precache-manifest.c510ebac144b6c67138ec840a6444734.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "419a888180775df1909ea43d8811cd70",
+    "url": "/reactResume/index.html"
+  },
+  {
+    "revision": "076e74120815f4ed5054",
+    "url": "/reactResume/static/css/main.762d5db9.chunk.css"
+  },
+  {
+    "revision": "225651986e638f1c2908",
+    "url": "/reactResume/static/js/2.4953daa0.chunk.js"
+  },
+  {
+    "revision": "e4279675a9c60179159c82343fcf3639",
+    "url": "/reactResume/static/js/2.4953daa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "076e74120815f4ed5054",
+    "url": "/reactResume/static/js/main.04c9ed9f.chunk.js"
+  },
+  {
+    "revision": "ba2e10ca387e85c405e9",
+    "url": "/reactResume/static/js/runtime-main.b7445227.js"
+  },
+  {
+    "revision": "fce168e61c31d0431e891b80ea4f6284",
+    "url": "/reactResume/static/media/avatar1.fce168e6.jpg"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/reactResume/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/reactResume/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/reactResume/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/reactResume/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/reactResume/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "20862f7e531c31fc7da34809c8192f15",
+    "url": "/reactResume/static/media/w1.20862f7e.jpg"
+  }
+]);
